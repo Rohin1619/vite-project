df1f78676d166915aaa12449e7c98fc34b4d1b26
Rohin Bharatee <rohinbharatee@gmail.com>
2023-08-30 12:57:21 +0545

    Random Id can be generated

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ListItem = (props) => {
-  const {taskObj } = props;
+  const { taskObj, onDelete, onEdit, isEditing, onEditClick } = props;
+  const [editedTask, setEditedTask] = useState(taskObj.task);
+
+  const handleDeleteClick = () => {
+    onDelete(taskObj.id);
+  };
+
+  const handleEditClick = () => {
+    if (isEditing) {
+      onEdit(taskObj.id, editedTask); 
+    } else {
+      onEditClick(taskObj.id);
+    }
+  };
+  
+
+  const handleTaskChange = (e) => {
+    setEditedTask(e.target.value);
+  };
 
   return (
     <div className="container" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', border: '1px solid', borderRadius: '12px', padding: '16px' }}>
       <div className="container-left">
-        <h2>{taskObj.task}</h2>
+        {isEditing ? (
+          <input
+            type="text"
+            placeholder="Edit Task"
+            value={editedTask}
+            onChange={handleTaskChange}
+          />
+          
+        ) : (
+          <>
+            <h2>{taskObj.id}</h2>
+          <h2>{taskObj.task}</h2>
+          </>
+
+        )}
       </div>
       <div className="container-right" style={{ margin: '16px' }}>
-        <button className="editBtn">
-          Edit
+        <button className="editBtn" onClick={handleEditClick}>
+          {isEditing ? 'Save' : 'Edit'}
         </button>
-        <button className="delBtn">
+        <button className="delBtn" onClick={handleDeleteClick}>
           Delete
         </button>
       </div>
@@ -21,4 +53,3 @@ const ListItem = (props) => {
 };
 
 export default ListItem;
-
--- src/Page/ToDoList.jsx
+++ src/Page/ToDoList.jsx
@@ -2,16 +2,40 @@ import React, { useState } from 'react';
 import ListItem from '../Components/ListItem';
 
 const ToDoList = () => {
-    const [id, setId] = useState({ id: '', newTask: '' }); 
+    const [task, setTask] = useState({ id: '', newTask: '' });
     const [tasksToDo, setTasksToDo] = useState([]);
+    const [editingTaskId, setEditingTaskId] = useState(null);
 
     const handleAddTask = () => {
-        if (id.newTask && id.id) { 
-            const newTaskObj = { task: id.newTask, id: id.id }; 
-            setTasksToDo([newTaskObj,...tasksToDo, ]);
-            setId({ id: '', newTask: '' }); 
+        const randomId = Math.floor(Math.random() * 10);
+        if (task.newTask) {
+            const newTaskObj = { task: task.newTask, id: randomId };
+            setTasksToDo([newTaskObj, ...tasksToDo]);
+            setTask({ id: '', newTask: '' });
         }
+        console.log({task: task.newTask});
+        console.log({id: randomId});
     };
+
+
+
+    const handleDeleteTask = (taskId) => {
+        const updatedTasks = tasksToDo.filter((taskObj) => taskObj.id !== taskId);
+        setTasksToDo(updatedTasks);
+    };
+
+    const handleEditTask = (taskId, editedTaskText) => {
+        const taskIndex = tasksToDo.findIndex((taskObj) => taskObj.id === taskId);
+        if (taskIndex !== -1) {
+          const updatedTasks = [...tasksToDo];
+          updatedTasks[taskIndex].task = editedTaskText;
+          setTasksToDo(updatedTasks);
+        }
+      };
+    
+      const handleEditClick = (taskId) => {
+        setEditingTaskId(taskId);
+      };
 
     return (
         <>
@@ -19,18 +43,25 @@ const ToDoList = () => {
                 name='task'
                 type="text"
                 placeholder="Enter Task"
-                value={id.newTask}
-                onChange={(e) => setId({ ...id, newTask: e.target.value })} 
+                value={task.newTask}
+                onChange={(e) => setTask({ ...task, newTask: e.target.value })}
             />
-            <input
+            {/* <input
                 type="text"
                 placeholder="Enter ID"
-                value={id.id}
-                onChange={(e) => setId({ ...id, id: e.target.value })} 
-            />
+                value={task.id}
+                onChange={(e) => setTask({ ...task, id: e.target.value })} 
+    />*/}
             <button onClick={handleAddTask}>Add Task</button>
             {tasksToDo.map((taskObj, index) => (
-                <ListItem key={index} taskObj={taskObj} />
+                <ListItem
+                    key={index}
+                    taskObj={taskObj}
+                    onDelete={handleDeleteTask}
+                    onEdit={handleEditTask}
+                    isEditing={editingTaskId === taskObj.id}
+                    onEditClick={handleEditClick}
+                />
             ))}
         </>
     );
